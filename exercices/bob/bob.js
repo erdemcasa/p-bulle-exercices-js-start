fb616af3b5e194219a28906f461ce554290af42d
Ökkes Erdem Köse <pl23ksc@eduvaud.ch>
2025-11-28 16:30:08 +0100

    feat(bob) Exercice fini.

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -4,5 +4,24 @@
 //
 
 export const hey = (message) => {
-  throw new Error('Remove this line and implement the function');
+  const response = ["Whatever.", "Whoa, chill out!", "Sure.", "Calm down, I know what I'm doing!", "Fine. Be that way!"]
+  const trimmedMessage = message.trim();
+  const isQuestion = trimmedMessage.endsWith("?");
+  const hasLetters = /[A-Z]/.test(trimmedMessage);
+  const isShooting = hasLetters && trimmedMessage === trimmedMessage.toUpperCase();
+
+  if (trimmedMessage === "") {
+    return response[4];
+  }
+  if (isShooting && isQuestion) {
+    return response[3];
+  }
+  if (isShooting) {
+    return response[1];
+  }
+  if (isQuestion) {
+    return response[2];
+  }
+  return response[0];
+
 };
